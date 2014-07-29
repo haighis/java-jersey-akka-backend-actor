9724cd634c2ec5b18c44fafdd68be9a24d262639
haighis <johnhaigh@Johns-MacBook-Pro.local>
2014-07-29 13:07:47 -0400

    Updated backend actor to write a sql table
    Updated backend actor to write a sql table with straight jdbc

--- src/main/java/com/paulsamiq/jersey2akka/Backend.java
+++ src/main/java/com/paulsamiq/jersey2akka/Backend.java
@@ -1,5 +1,11 @@
 package com.paulsamiq.jersey2akka;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
@@ -14,20 +20,45 @@ public class Backend extends UntypedActor {
 	}
 	
 	@Override
-	public void preStart() {
-	    log.debug("starting");
+	public void preStart() throws Exception {
+		
+		// Create db connection
+		log.debug("starting");
 	}
 	
-    public void onReceive(Object message) {
+    public void onReceive(Object message) throws Exception {
         
         if(message instanceof LogMessage)
         {
+        	Connection conn = getConnectionMySql();
+    	    Statement stmt = conn.createStatement();
+    	    String title = ((LogMessage)message).getTitle();
+    	    
+    	    stmt.executeUpdate("INSERT INTO LogMessage (message) VALUES('" + title +"');");
+
+    	    stmt.close();
+    	    conn.close();
         	System.out.println("IN backend");
-        	System.out.println("IN backend message is: " + ((LogMessage)message).getTitle() );
+        	System.out.println("IN backend message is: " +  title );
         }
         else unhandled(message);
         {
         
         }
+    }
+    
+    private Connection getConnection() throws Exception {
+        Class.forName("org.hsqldb.jdbcDriver");
+        String url = "jdbc:hsqldb:mem:data/tutorial";
+
+        return DriverManager.getConnection(url, "sa", "");
+      }
+    
+    private Connection getConnectionMySql() throws Exception
+    {
+    	Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost/AuditDb?user=root&password=WbyXs0YE";
+
+        return DriverManager.getConnection(url);
     }
 }
